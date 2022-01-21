99e5e59a5072a8675d55e9d4f5b23ce2c1f8f866
Aleksey Plutaev <plutaef@gmail.com>
2022-01-22 00:38:49 +0500

    blackjack mechanics

--- dist/script.js
+++ dist/script.js
@@ -2765,8 +2765,8 @@ function newRound(winnerSelector, newGameSelector, modalSelector, bjModalSelecto
       reward = document.querySelector(rewardSelector);
   var deck = ['10-s', 'a-s', 'a-h', '5-s', '6-s', '7-s', '8-s', '9-s', '2-s', 'j-s', 'q-s', 'k-s', '3-s', '2-h', '3-h', '4-h', '5-h', '6-h', '7-h', '8-h', '9-h', '10-h', 'j-h', 'q-h', 'k-h', '4-s', '2-c', '3-c', '4-c', '5-c', '6-c', '7-c', '8-c', '9-c', '10-c', 'j-c', 'q-c', 'k-c', 'a-c', '2-d', '3-d', '4-d', '5-d', '6-d', '7-d', '8-d', '9-d', '10-d', 'j-d', 'q-d', 'k-d', 'a-d'];
   var yourHand = [],
-      dealerHand = []; //deck = shuffle(deck);
-
+      dealerHand = [];
+  deck = Object(_shuffle__WEBPACK_IMPORTED_MODULE_3__["shuffle"])(deck);
   localStorage.setItem('deck', JSON.stringify(deck));
   localStorage.setItem('yourHand', JSON.stringify(yourHand));
   localStorage.setItem('dealerHand', JSON.stringify(dealerHand));
